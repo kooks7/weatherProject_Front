2f7053616477685458c27f7a113390394250058a
kooks7 <busanminjae@naver.com>
2020-04-10 15:57:15 +0900

    Add First Setting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    forecast: {}
+  };
+  getData = async () => {
+    let city = 'busan';
+    const data = await axios.get(`http://localhost:4000/weather/${city}`);
+
+    console.log(data);
+    // console.log(data.data.city.name);
+    // for (let d of weatherData) {
+    //   let time = new Date(d.dt * 1000);
+    //   d.dt = `${time.getFullYear().toString()}년 ${(
+    //     time.getMonth() + 1
+    //   ).toString()}월 ${time.getDate().toString()}일 ${time.getHours()}:00 시`;
+    //   d.main.temp = Math.floor(d.main.temp - 274.0);
+    //   d.main.feels_like = Math.floor(d.main.feels_like - 274.0);
+    //   // d.main.weather[0].main = d.main.weather[0].main;
+    // }
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading..' : 'We are Ready'}</div>;
+  }
 }
 
 export default App;
